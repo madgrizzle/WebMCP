58e83519d574897efeb3a8489131d0d0fd793329
madgrizzle <john@thehogans.name>
2018-12-05 22:24:07 -0500

    gui cleanup.. desktop still wip

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-docker run -it -v $HOME/.WebControl:/root/.WebControl -v /var/run/docker.sock:/var/run/docker.sock -p 5001:5001 -e HOST_HOME=$HOME --network="host" --privileged madgrizzle/webmcp
+docker run -it -v $HOME/.WebControl:/root/.WebControl -v /var/run/docker.sock:/var/run/docker.sock -p 5001:5001 -e HOST_HOME=$HOME --network=host --privileged madgrizzle/webmcp
