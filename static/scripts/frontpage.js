e9ecfa24e7c34fdb03c8fca9eaab569617272bea
John Hogan <john@thehogans.name>
2018-12-06 11:10:19 -0500

    "final" gui update.

---
+++ static/scripts/frontpage.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+/*   var webcontrolMessage = document.getElementById('webcontrolMessage');
+   var webmcpMessage = document.getElementById('uiMessage');
+   webcontrolMessage.scrollTop = webcontrolMessage.scrollHeight;
+   webmcpMessage.scrollTop = webmcpMessage.scrollHeight;
+   */
+});
